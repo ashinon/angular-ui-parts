0070ad606826e22e577035759264bcb2b2a5b3ae
ashinon <nanase.a.1071607@gmail.com>
2022-12-13 23:16:49 +0900

    [add][clean] inputComponentをパラメータで数値入力欄に出来るように修正、appComponentに使用例を追加、他整理

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+type option = { value: string, label: string };
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-ui-parts';
+
+  constructor() {}
+
+  testOptions: option[] = [
+    { value: '1', label: 'aaa' },
+    { value: '2', label: 'bbb' },
+    { value: '3', label: 'ccc' },
+  ];
+
 }
